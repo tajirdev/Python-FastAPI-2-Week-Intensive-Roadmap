e1c4deeb563f3b6222877b96144ff3c1453d0394
dottomtundu9-MTUNDU <dottomtundu9@gmail.com>
2026-06-04 17:02:18 +0300

    add error handling

--- pwd_hashing/main.py
+++ pwd_hashing/main.py
@@ -11,11 +11,17 @@ app= FastAPI()
 @app.post("/user/register",tags=["user blog"])
 def register_user(dotto:schema.User,db:Session=Depends(get_db)):
     hashed_password=hash.hash_password(dotto.password)
+    
     register = models.User(
         username = dotto.username,
         email = dotto.email,
         password= hashed_password
     )
+    if register.username == dotto.username:
+        raise HTTPException(status_code=status.HTTP_208_ALREADY_REPORTED,detail="a user name already exist")
+    if register.email== dotto.email:
+        raise HTTPException(status_code=status.HTTP_208_ALREADY_REPORTED,detail="an email already exist")
+    db.add(register)
     db.add(register)
     db.commit()
     db.refresh(register)
@@ -41,12 +47,14 @@ def update(id, request:schema.User,db:Session=Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"A user with id {id} not found")
     update_user.username = request.username
     update_user.email = request.email
-    update_user.passsword = request.passwoed   
+    update_user.passsword = request.password  
+    db.commit()
+    db.refresh(update_user) 
     return f"A user with id {id} updated"
 
 @app.delete("/uesrDelete/{id}",tags=["user blog"])
 def delete(id,db:Session=Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id==id).delete(synchronize_session=True)
+    user = db.query(models.User).filter(models.User.id==id).delete(synchronize_session=False)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"A user with id {id} not found")
     db.commit()
